4d051d7a4d19f4f6b43276c8483b9c6b85cd856e
Matt Leeper <leepermatt@gmail.com>
2023-09-18 11:55:33 -0700

    week 2 hw

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        string response;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            
+            int guessNumber = -1;
+            
+            while (guessNumber != magicNumber) 
+            {
+                Console.WriteLine("What is your guess for the magic number?");
+            
+                guessNumber = int.Parse(Console.ReadLine());
+                
+
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("The Magic number is Higher. Guess again.");
+                    
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("The Magic number is lower. Guess again.");
+                    
+                }
+                else 
+                {
+                    Console.WriteLine ($"You guessed the right number!");
+                }
+            }
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
