9e50ad7a3d14756c26c0d59640f035617dd5d17a
Matt Leeper <leepermatt@gmail.com>
2023-09-22 10:01:18 -0700

    week 2 pro 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int inputNumber =0;
+        
+;
+        do
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished");
+            string userResponse = Console.ReadLine();
+            inputNumber = int.Parse(userResponse);
+            if (inputNumber != 0);
+            numbers.Add(inputNumber);
+        } while (inputNumber != 0 );
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+                int maxNumber = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                
+                maxNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {maxNumber}");
     }
 }
